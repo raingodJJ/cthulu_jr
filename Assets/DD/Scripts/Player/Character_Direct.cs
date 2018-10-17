956f9cc2290ecce747e04e1ad049e9cd948a6286
raingodJJ <outoraame@gmail.com>
2018-10-18 07:12:19 +0900

    asdf

---
+++ Assets/DD/Scripts/Player/Character_Direct.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TentacleState
+{
+    idle,
+    grabbing,
+    holding,
+    throwing
+}
+
+[RequireComponent(typeof(CharacterController))]
+public class Character_Direct : MonoBehaviour
+{
+
+    [Header("Input Axes")]
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+    public string horizontalThumbstick = "Horizontal_Thumbstick";
+    public string verticalThumbstick = "Vertical_Thumbstick";
+    public string attackAxis = "Fire1";
+
+    [Header("Movement Properties")]
+    public float speed = 10f;
+
+    [Header("AimProperties")]
+    public float angularSpeed = 5f;
+    public Camera MainCamera;
+    public Vector3 _aim;
+
+    [Header("Tentacle Properties")]
+    public Transform tentaclePoint;
+    public TentacleState tentacleState = TentacleState.idle;
+    public GameObject tentacle;
+    private TentacleBehavior _tentacle;
+
+
+    private CharacterController _characterController;
+    private Vector3 _characterVelocity = Vector3.zero;
+    private Vector3 _thumbstickVector = Vector3.zero;
+
+    private bool _canMove = true;
+    private bool _canAim = true;
+
+    private Plane _groundPlane;
+
+    // Use this for initialization
+    void Start () {
+        _characterController = this.GetComponent<CharacterController>();
+        _groundPlane = new Plane(Vector3.up, this.transform.position);
+        _tentacle = tentacle.GetComponent<TentacleBehavior>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_canMove)
+        {
+            Move();
+        }
+        else
+        {
+            _characterVelocity = Vector3.zero;
+        }
+        if (_canAim)
+        {
+            Aim();
+        }
+        Tentacle(tentacleState);
+        _characterController.Move(_characterVelocity * Time.deltaTime);
+	}
+
+    private void Move()
+    {
+        Vector3 direction = Vector3.zero;
+
+        direction.x = Input.GetAxis(horizontalAxis);
+        direction.z = Input.GetAxis(verticalAxis);
+
+        _characterVelocity = direction * speed;
+    }
+
+    private void Aim()
+    {
+        Ray screenRay = MainCamera.ScreenPointToRay(Input.mousePosition);
+        float intersection = 0.0f;
+
+        // Set the raycast plane to the position of the player facing up
+        _groundPlane.SetNormalAndPosition(Vector3.up, this.transform.position);
+
+        // Perform a raycast to track the intersection distance of the ray
+        if (_groundPlane.Raycast(screenRay, out intersection))
+        {
+            // Calculate the hit point on the plane and set the look at of the character transform
+            _aim = screenRay.GetPoint(intersection);
+            this.transform.LookAt(_aim);
+        }
+    }
+
+    private void Tentacle(TentacleState state)
+    {
+        if (state == TentacleState.idle)
+        {
+            if (Input.GetAxis(attackAxis) > 0.5f)
+            {
+                _tentacle.Invoke("Fire", 0.0f);
+            }
+            else
+            {
+                _canMove = true;
+                _canAim = true;
+            }
+        }
+        else if (state == TentacleState.grabbing)
+        {
+            _canMove = false;
+            _canAim = false;
+        }
+        else if (state == TentacleState.holding)
+        {
+            if (Input.GetAxis(attackAxis) > 0.5f)
+            {
+                _tentacle.Invoke("Fire", 0.0f);
+            }
+            else
+            {
+                _canMove = true;
+                _canAim = true;
+            }
+        }
+        this.tentacleState = _tentacle.state;
+    }
+}
---
+++ Assets/DD/Scripts/Player/TentacleBehavior.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleBehavior : MonoBehaviour
+{
+    public float maxRange;
+    public float maxSpeed;
+    public GameObject player;
+    private Character_Direct _player;
+    public TentacleState state = TentacleState.idle;
+    public Vector3 target;
+    private Vector3 velocity;
+    public GameObject holding;
+    private int grabCounter = 0;
+    public int grabDuration;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _player = player.GetComponent<Character_Direct>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (state == TentacleState.grabbing)
+        {
+            if (grabCounter != grabDuration/2)
+            {
+                velocity = target - transform.position;
+                Vector3 pos;
+                pos.x = transform.position.x + velocity.x * Time.deltaTime * 11;
+                pos.y = transform.position.y;
+                pos.z = transform.position.z + velocity.z * Time.deltaTime * 11;
+                transform.position = pos;
+                grabCounter++;
+                if (grabCounter == grabDuration)
+                {
+                    transform.position = _player.tentaclePoint.transform.position;
+                    grabCounter = 0;
+                    state = TentacleState.idle;
+                }
+            }
+            else if (grabCounter == grabDuration/2)
+            {
+                target = _player.tentaclePoint.transform.position;
+                grabCounter++;
+            }
+        }
+        else if (state == TentacleState.holding)
+        {
+            holding.transform.position = this.transform.position;
+            velocity = _player._aim - transform.position;
+            Vector3 pos;
+            pos.x = transform.position.x + velocity.x * Time.deltaTime * 11;
+            pos.y = transform.position.y;
+            pos.z = transform.position.z + velocity.z * Time.deltaTime * 11;
+            transform.position = pos;
+        }
+        else if (state == TentacleState.throwing)
+        {
+            holding = null;
+            state = TentacleState.idle;
+        }
+	}
+
+    void Fire()
+    {
+        if (state == TentacleState.idle)
+        {
+            state = TentacleState.grabbing;
+            target = _player._aim;
+            //target too far
+        }
+        else if (state == TentacleState.holding)
+        {
+            state = TentacleState.throwing;
+            target = _player._aim;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state == TentacleState.grabbing)
+        {
+            if (collision.gameObject.tag == "Moveable")
+            {
+                holding = collision.gameObject;
+                Rigidbody rigBod = holding.GetComponent<Rigidbody>();
+                rigBod.isKinematic = true;
+                state = TentacleState.holding;
+            }
+        }
+    }
+}
